4363a5b26870c8695a54f5b85fda29c42f45659e
amohan00 <arm198@pitt.edu>
2020-12-07 22:28:49 -0500

    Add files via upload

---
+++ DNID/posture.py
@@ -0,0 +1,27 @@
+#Python program to scrape website  
+#and save data from website 
+import requests 
+from bs4 import BeautifulSoup 
+import csv 
+
+def pitt_posture():
+	# Download HTML from website
+	URL = "https://www.coronavirus.pitt.edu/"
+	page = requests.get(URL) 
+
+	# Soup Objects to find data 
+	soup = BeautifulSoup(page.content, 'html.parser')
+	section = soup.find(class_="views-field views-field-views-conditional")
+	group = section.find_all('div', class_='')
+	postures = []
+	for x in group:
+		# Print risk posture
+		stat = x.find('h4')
+		temp = stat.get_text()
+		postures.append(temp)
+		# Print campuses
+		campus = x.find_all('li')
+		for y in campus:
+			temp = y.get_text()
+			postures.append(temp)
+	return postures
